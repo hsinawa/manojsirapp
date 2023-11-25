4a95f158b7ee1e11059e746dcff04020825b713a
hsinawa <awanishmishra003@gmail.com>
2023-11-26 00:07:54 +0530

    Subsection added

--- client/src/Screen/StudentLectureCategory.js
+++ client/src/Screen/StudentLectureCategory.js
@@ -6,6 +6,14 @@ import {GetAllLectureReducer} from '../Reducers/lectureReducer'
 import { Alert, CircularProgress } from '@mui/material';
 import LectureCard from '../Component/LectureCard';
 
+
+//MUI
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 const StudentLectureCategory = () => {
     const {subject} = useParams();
     const stand = JSON.parse(localStorage.getItem('student')).standard
@@ -16,14 +24,46 @@ const StudentLectureCategory = () => {
     } , [dispatch] )
 
     const {loading,lectures, error} = useSelector(state=>state.GetAllLectureReducer)
-console.log('Lectures are', lectures)
+
+    const groupedData = lectures?.reduce((result, current) => {
+        const { chapterName, ...rest } = current;
+        (result[chapterName] = result[chapterName] || []).push(rest);
+        return result;
+      }, {});
+      
+    var IterativeData = Object.entries(groupedData)
+
+
+
     return(
         <div> 
            {loading&&(<CircularProgress/>)}
            {error&&(<Alert severity="error"> OOPS! Something Went Wrong </Alert>)}
-           {lectures&&lectures.map((i,key)=>{
-               return <LectureCard i={i} key={key} />
-           })}
+           {
+               IterativeData&&IterativeData?.map(i=>{
+                   return       <Accordion style={{
+                       width:'90%',
+                       marginLeft:'auto', marginRight:'auto'
+                   }} >
+                   <AccordionSummary
+                     expandIcon={<ExpandMoreIcon />}
+                     aria-controls="panel1a-content"
+                     id="panel1a-header"
+                   >
+                     <Typography> {i[0]} </Typography>
+                   </AccordionSummary>
+                   {
+                       i[1]?.map((j,key)=>{
+                           return <LectureCard i={j} key={key} />
+                       })
+                   }
+                   <AccordionDetails>
+                    
+                   </AccordionDetails>
+                 </Accordion>
+               })
+           }
+          
         </div>
     )
 }
